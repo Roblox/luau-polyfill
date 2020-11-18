041209d24dd6c5e6672b6d0374a59dde983454fa
Anonymous Author <roflection-bot@roblox.com>
2020-11-17 16:05:35 -0800

    add more test cases

--- src/Object/___tests__/keys.spec.lua
+++ src/Object/___tests__/keys.spec.lua
@@ -1,3 +1,4 @@
+--!nocheck
 return function()
 	local keys = require(script.Parent.Parent.keys)
 
@@ -8,9 +9,30 @@ return function()
 	it("returns an array with the table keys", function()
 		local t = { foo = true, bar = false }
 		local result = keys(t)
-		expect(#result).to.equal(2)
+		expect(#result).toEqual(2)
 		table.sort(result)
-		expect(result[1]).to.equal('bar')
-		expect(result[2]).to.equal('foo')
+		expect(result).toEqual({"bar", "foo"})
+	end)
+
+	it("returns an empty array given a number", function()
+		expect(keys(1)).toEqual({})
+	end)
+
+	it("returns an empty array given boolean", function()
+		expect(keys(true)).toEqual({})
+		expect(keys(false)).toEqual({})
+	end)
+
+	it("throws when given nil", function()
+		expect(function()
+			keys(nil)
+		end).to.throw("cannot extract keys from a nil value")
+	end)
+
+	-- deviation: JS has this behavior, which we don't specifically need now.
+	-- To not risk making the function significantly slower, this behavior is
+	-- not implemented
+	itSKIP("returns an array of stringified index given an array", function()
+		expect(keys({true, false, true})).toEqual({"1", "2", "3"})
 	end)
 end
