4f7439c5f67c89bc0b57288035a03e61d47fa5b4
Anonymous Author <roflection-bot@roblox.com>
2021-02-02 13:10:10 -0500

    Changing Error Polyfill to be a Table (#25)
    Adding the ability for the Error polyfill to be inheritable

--- src/Error/init.lua
+++ src/Error/init.lua
@@ -1,14 +1,48 @@
+--!nocheck
+local Error = {}
+
 local DEFAULT_NAME = "Error"
 
-local function Error(message)
+function Error.new(_, message)
 	return setmetatable({
 		name = DEFAULT_NAME,
 		message = message,
 	}, {
 		__tostring = function(self)
 			return string.format("%s: %s", tostring(self.name), tostring(self.message))
-		end,
+		end
 	})
 end
 
-return Error
+function Error.extend(parentError, childErrorName)
+	local ChildError = {}
+
+	ChildError.__index = parentError
+	ChildError.__tostring = function(self)
+		return string.format("%s: %s", tostring(self.name), tostring(self.message))
+	end
+
+	function ChildError.new(_, message)
+		return setmetatable({
+			name = childErrorName,
+			message = message,
+		}, ChildError)
+	end
+
+	setmetatable(ChildError, {
+		__call = ChildError.new,
+		__index = parentError,
+		__tostring = function(self)
+			return tostring(childErrorName)
+		end
+	})
+
+	return ChildError
+end
+
+return setmetatable(Error, {
+	__call = Error.new,
+	__tostring = function(self)
+		return tostring(DEFAULT_NAME)
+	end
+})
