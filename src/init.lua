d24a8b7321802affaf0c2d333e738a7492f39729
Anonymous Author <roflection-bot@roblox.com>
2021-02-12 11:56:43 -0800

    Add polyfill for Set class (#33)

--- src/init.lua
+++ src/init.lua
@@ -1,4 +1,8 @@
 local Timers = require(script.Timers)
+local Set = require(script.Set)
+
+export type Array<T> = { [number]: T }
+export type Set<T> = Set.Set<T>
 
 return {
 	Array = require(script.Array),
@@ -9,6 +13,7 @@ return {
 	Math = require(script.Math),
 	Number = require(script.Number),
 	Object = require(script.Object),
+	Set = Set,
 	RegExp = require(script.RegExp),
 	String = require(script.String),
 	Symbol = require(script.Symbol),
